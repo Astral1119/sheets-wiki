27e8525293f4c5e3576fbdbb8ddff29b93f00b4d
Astral Café <astral@astral.cafe>
2024-04-29 13:55:12 -0400

    Trimming down eleventy.js even further.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
 // Plugins from Obsidian Garden
 const slugify = require("@sindresorhus/slugify");
 const markdownIt = require("markdown-it");
-const fs = require("fs");
-const matter = require("gray-matter");
-const tocPlugin = require("eleventy-plugin-nesting-toc");
 const { parse } = require("node-html-parser");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const { headerToId, namedHeadingsFilter } = require("./src/helpers/utils");
@@ -55,147 +52,10 @@ module.exports = function (eleventyConfig) {
       closeMarker: "```",
     })
     .use(namedHeadingsFilter)
-    .use(function (md) {
-      //https://github.com/DCsunset/markdown-it-mermaid-plugin
-      const origFenceRule =
-        md.renderer.rules.fence ||
-        function (tokens, idx, options, env, self) {
-          return self.renderToken(tokens, idx, options, env, self);
-        };
-      md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-        const token = tokens[idx];
-        if (token.info === "mermaid") {
-          const code = token.content.trim();
-          return `<pre class="mermaid">${code}</pre>`;
-        }
-        if (token.info === "transclusion") {
-          const code = token.content.trim();
-          return `<div class="transclusion">${md.render(code)}</div>`;
-        }
-        if (token.info.startsWith("ad-")) {
-          const code = token.content.trim();
-          const parts = code.split("\n")
-          let titleLine;
-          let collapse;
-          let collapsible = false
-          let collapsed = true
-          let icon;
-          let color;
-          let nbLinesToSkip = 0
-          for (let i = 0; i < 4; i++) {
-            if (parts[i] && parts[i].trim()) {
-              let line = parts[i] && parts[i].trim().toLowerCase()
-              if (line.startsWith("title:")) {
-                titleLine = line.substring(6);
-                nbLinesToSkip++;
-              } else if (line.startsWith("icon:")) {
-                icon = line.substring(5);
-                nbLinesToSkip++;
-              } else if (line.startsWith("collapse:")) {
-                collapsible = true
-                collapse = line.substring(9);
-                if (collapse && collapse.trim().toLowerCase() == 'open') {
-                  collapsed = false
-                }
-                nbLinesToSkip++;
-              } else if (line.startsWith("color:")) {
-                color = line.substring(6);
-                nbLinesToSkip++;
-              }
-            }
-          }
-          const foldDiv = collapsible ? `<div class="callout-fold">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-chevron-down">
-              <polyline points="6 9 12 15 18 9"></polyline>
-          </svg>
-          </div>` : "";
-          const titleDiv = titleLine
-            ? `<div class="callout-title"><div class="callout-title-inner">${titleLine}</div>${foldDiv}</div>`
-            : "";
-          let collapseClasses = titleLine && collapsible ? 'is-collapsible' : ''
-          if (collapsible && collapsed) {
-            collapseClasses += " is-collapsed"
-          }
-
-          let res = `<div data-callout-metadata class="callout ${collapseClasses}" data-callout="${token.info.substring(3)
-            }">${titleDiv}\n<div class="callout-content">${md.render(
-              parts.slice(nbLinesToSkip).join("\n")
-            )}</div></div>`;
-          return res
-        }
-
-        // Other languages
-        return origFenceRule(tokens, idx, options, env, slf);
-      };
-
-      const defaultImageRule =
-        md.renderer.rules.image ||
-        function (tokens, idx, options, env, self) {
-          return self.renderToken(tokens, idx, options, env, self);
-        };
-      md.renderer.rules.image = (tokens, idx, options, env, self) => {
-        const imageName = tokens[idx].content;
-        //"image.png|metadata?|width"
-        const [fileName, ...widthAndMetaData] = imageName.split("|");
-        const lastValue = widthAndMetaData[widthAndMetaData.length - 1];
-        const lastValueIsNumber = !isNaN(lastValue);
-        const width = lastValueIsNumber ? lastValue : null;
-
-        let metaData = "";
-        if (widthAndMetaData.length > 1) {
-          metaData = widthAndMetaData.slice(0, widthAndMetaData.length - 1).join(" ");
-        }
-
-        if (!lastValueIsNumber) {
-          metaData += ` ${lastValue}`;
-        }
-
-        if (width) {
-          const widthIndex = tokens[idx].attrIndex("width");
-          const widthAttr = `${width}px`;
-          if (widthIndex < 0) {
-            tokens[idx].attrPush(["width", widthAttr]);
-          } else {
-            tokens[idx].attrs[widthIndex][1] = widthAttr;
-          }
-        }
-
-        return defaultImageRule(tokens, idx, options, env, self);
-      };
-
-      const defaultLinkRule =
-        md.renderer.rules.link_open ||
-        function (tokens, idx, options, env, self) {
-          return self.renderToken(tokens, idx, options, env, self);
-        };
-      md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-        const aIndex = tokens[idx].attrIndex("target");
-        const classIndex = tokens[idx].attrIndex("class");
-
-        if (aIndex < 0) {
-          tokens[idx].attrPush(["target", "_blank"]);
-        } else {
-          tokens[idx].attrs[aIndex][1] = "_blank";
-        }
-
-        if (classIndex < 0) {
-          tokens[idx].attrPush(["class", "external-link"]);
-        } else {
-          tokens[idx].attrs[classIndex][1] = "external-link";
-        }
-
-        return defaultLinkRule(tokens, idx, options, env, self);
-      };
-    })
     .use(userMarkdownSetup);
 
   // Actually set the library.
   eleventyConfig.setLibrary("md", markdownLib);
-
-  // Obsidian Garden Date. MAY NEED TO BE RESOLVED WITH DATE SCHEME BELOW.
-  eleventyConfig.addFilter("isoDate", function (date) {
-    return date && date.toISOString();
-  });
 
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
@@ -262,10 +122,6 @@ module.exports = function (eleventyConfig) {
     return str && parsed.innerHTML;
   });
 
-  // This is where the Obsidian Garden picture and table stuff would go. I'm opting to skip it for now since
-  // it seems like the functionality included with NEAT works well enough.
-
-  // I'm using the NEAT minifier. This is where the Obsidian Garden minifier would go.
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
@@ -277,7 +133,6 @@ module.exports = function (eleventyConfig) {
       });
       return minified;
     }
-
     return content;
   });
 
@@ -287,33 +142,8 @@ module.exports = function (eleventyConfig) {
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
 
-
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
-
-  // Table of Content
-  eleventyConfig.addPlugin(tocPlugin, {
-    ul: true,
-    tags: ["h1", "h2", "h3", "h4", "h5", "h6"],
-  });
-
-  // More Obsidian Garden stuff. I don't actually know what these are used for.
-  eleventyConfig.addFilter("dateToZulu", function (date) {
-    if (!date) return "";
-    return new Date(date).toISOString("dd-MM-yyyyTHH:mm:ssZ");
-  });
-
-  eleventyConfig.addFilter("jsonify", function (variable) {
-    return JSON.stringify(variable) || '""';
-  });
-  eleventyConfig.addFilter("validJson", function (variable) {
-    if (Array.isArray(variable)) {
-      return variable.map((x) => x.replaceAll("\\", "\\\\")).join(",");
-    } else if (typeof variable === "string") {
-      return variable.replaceAll("\\", "\\\\");
-    }
-    return variable;
-  });
 
   eleventyConfig.addPlugin(pluginRss, {
     posthtmlRenderOptions: {
@@ -327,15 +157,13 @@ module.exports = function (eleventyConfig) {
   userEleventySetup(eleventyConfig);
 
   // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
+    "./node_modules/prismjs/themes/prism-tomorrow.css": "./static/css/prism-tomorrow.css",
   });
 
   // Copy Image Folder to /_site
@@ -346,8 +174,6 @@ module.exports = function (eleventyConfig) {
 
   // Optimizing SEO
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
-
-  // Optimizing SEO
   eleventyConfig.addPassthroughCopy("./src/_headers");
 
   // Add aliasing, courtesy of Boehs
---
+++ src/blog/posts/markdown-sandbox.md
@@ -0,0 +1,21 @@
+---
+title: Markdown Sandbox
+description: This blog post is a sandbox for markdown tools.
+author: sheets.wiki Staff
+date: 2024-04-29
+tags:
+  - advice
+---
+> [!WARNING]
+> This is a sandbox for the sheets.wiki markdown editor.
+
+$\frac{x}{y} \times \pi$
+
+```plantuml
+@startuml firstDiagram
+
+Alice -> Bob: Hello
+Bob -> Alice: Hi!
+		
+@enduml
+```
